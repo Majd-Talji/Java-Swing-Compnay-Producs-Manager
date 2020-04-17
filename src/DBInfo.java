a65c3f203a27d7311452909889b63cdb49810dc9
Majd-Talji <53352101+Majd-Talji@users.noreply.github.com>
2020-04-17 07:19:19 +0200

    first commit

---
+++ src/DBInfo.java
@@ -0,0 +1,17 @@
+/**
+ *
+ * @author Majd Talji <en.majd.talji@gmail.com>
+ */
+public interface DBInfo {
+    
+    String DB_NAME = "jdbc:mysql://localhost/products_db";
+    
+    String ENCODING = "?useUnicode=yes&characterEncoding=UTF-8";
+    
+    String DB_NAME_WITH_ENCODING = DB_NAME + ENCODING;
+    
+    String USER = "root";
+    
+    String PASSWORD = "";
+    
+}
